059a67d03924d9c5e6fc5c60dc269484bbf96ff6
MaycolMont <elmaycolm1403@gmail.com>
2026-05-25 12:54:29 -0500

    grid focus point added

--- scripts/grid.gd
+++ scripts/grid.gd
@@ -1,61 +1,40 @@
 extends Control
 
-var min_square_size: int = 15
-var max_square_size: int = 30
-var square_size: int = 15
-var scale_factor: float = 1.0
-var grid_color: Color = Color(1.0, 1.0, 1.0, 1.0)
+@export var square_size: int = 15
 var screen_size : Vector2
+
+var grid_color: Color = Color(1.0, 1.0, 1.0, 1.0)
+var _highlight_color: Color = Color(0.3, 0.6, 1.0, 1.0)
+
 var vertical_lines : Array[float] = []
 var horizontal_lines : Array[float] = []
 var _vertical_nodes : Array[Line2D] = []
 var _horizontal_nodes : Array[Line2D] = []
-var _highlight_color: Color = Color(0.3, 0.6, 1.0, 1.0)
-var _offset: Vector2
 
-func hide_grid()-> void:
-	for node in _vertical_nodes:
-		node.hide()
-	
-	for node in _vertical_nodes:
-		node.hide()
-		
+#region private
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
 	_draw_lines()
 
 func _process(delta: float) -> void:
+	_update_highlight_lines()
+	
+## Return the line's idx
+func _get_focus_idx() -> Vector2:
 	var mouse = get_global_mouse_position()
-	var start_x = fmod(_offset.x, square_size)
-	if start_x < 0:
-		start_x += square_size
-	var start_y = fmod(_offset.y, square_size)
-	if start_y < 0:
-		start_y += square_size
 
 	var v_size = _vertical_nodes.size()
 	var h_size = _horizontal_nodes.size()
-	var v_idx = -1 if v_size == 0 else int(clamp(round((mouse.x - start_x) / square_size), 0, v_size - 1))
-	var h_idx = -1 if h_size == 0 else int(clamp(round((mouse.y - start_y) / square_size), 0, h_size - 1))
+	var v_idx = -1 if v_size == 0 else int(clamp(round(mouse.x / square_size), 0, v_size - 1))
+	var h_idx = -1 if h_size == 0 else int(clamp(round(mouse.y / square_size), 0, h_size - 1))
+	return Vector2(v_idx, h_idx)
 
+func _update_highlight_lines() -> void:
+	var focus_idx : Vector2 = _get_focus_idx()
 	for i in _vertical_nodes.size():
-		_vertical_nodes[i].default_color = _highlight_color if i == v_idx else grid_color
+		_vertical_nodes[i].default_color = _highlight_color if i == focus_idx.x else grid_color
 	for i in _horizontal_nodes.size():
-		_horizontal_nodes[i].default_color = _highlight_color if i == h_idx else grid_color
-
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("zoom in"):
-		_zoom_in()
-	if event.is_action_pressed("zoom out"):
-		_zoom_out()
-
-func _clear_lines() -> void:
-	for node in _vertical_nodes + _horizontal_nodes:
-		node.queue_free()
-	vertical_lines.clear()
-	horizontal_lines.clear()
-	_vertical_nodes.clear()
-	_horizontal_nodes.clear()
+		_horizontal_nodes[i].default_color = _highlight_color if i == focus_idx.y else grid_color
 
 func _draw_line(start: Vector2, end: Vector2) -> Line2D:
 	var line = Line2D.new()
@@ -70,49 +49,22 @@ func _draw_lines() -> void:
 	var width = screen_size.x
 	var height = screen_size.y
 
-	var start_x = fmod(_offset.x, square_size)
-	if start_x < 0:
-		start_x += square_size
-	var start_y = fmod(_offset.y, square_size)
-	if start_y < 0:
-		start_y += square_size
-
-	for x in range(start_x, width + 1, square_size):
+	for x in range(0, width + 1, square_size):
 		vertical_lines.append(x)
 		_vertical_nodes.append(_draw_line(Vector2(x, 0),
 				Vector2(x, height)))
 
-	for y in range(start_y, height + 1, square_size):
+	for y in range(0, height + 1, square_size):
 		horizontal_lines.append(y)
 		_horizontal_nodes.append(_draw_line(Vector2(0, y),
 				Vector2(width, y)))
 
-func _zoom_in() -> void:
-	var mouse = get_global_mouse_position()
-	var grid_x = (mouse.x - _offset.x) / square_size
-	var grid_y = (mouse.y - _offset.y) / square_size
+#endregion
 
-	square_size += scale_factor
-	if square_size > max_square_size:
-		square_size = min_square_size
+#region public
 
-	_offset.x = mouse.x - grid_x * square_size
-	_offset.y = mouse.y - grid_y * square_size
-
-	_clear_lines()
-	_draw_lines()
-
-func _zoom_out() -> void:
-	var mouse = get_global_mouse_position()
-	var grid_x = (mouse.x - _offset.x) / square_size
-	var grid_y = (mouse.y - _offset.y) / square_size
-
-	square_size -= scale_factor
-	if square_size < min_square_size:
-		square_size = max_square_size
-
-	_offset.x = mouse.x - grid_x * square_size
-	_offset.y = mouse.y - grid_y * square_size
-
-	_clear_lines()
-	_draw_lines()
+func get_focus_point() -> Vector2:
+	var focus_idx : Vector2 = _get_focus_idx()
+	return Vector2(focus_idx.x, focus_idx.y)
+	
+#endregion
